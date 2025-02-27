d7d7cbc4c72f79043067b14be7bf56447de07b5e
Nikolas <131282424+N1k0145@users.noreply.github.com>
2025-02-27 14:55:13 -0300

    Add files via upload

--- PrimeiroExercicio.java
+++ PrimeiroExercicio.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class Exercicio01 {
     
-    // Método para ler uma nota
     public static double lerNota() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a nota: ");
         return scanner.nextDouble();
     }
     
-    // Método para calcular a média
     public static double calcularMedia(double nota1, double nota2) {
         return (nota1 + nota2) / 2;
     }
@@ -17,18 +15,14 @@ public class Exercicio01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura do nome do aluno
         System.out.println("Digite o nome do aluno: ");
         String nome = scanner.nextLine();
 
-        // Leitura das notas usando o método lerNota
         double nota1 = lerNota();
         double nota2 = lerNota();
 
-        // Cálculo da média
         double media = calcularMedia(nota1, nota2);
 
-        // Exibindo a média e o status de aprovação
         System.out.println("Média do aluno " + nome + ": " + media);
         if (media >= 7.0) {
             System.out.println("Aluno aprovado!");
